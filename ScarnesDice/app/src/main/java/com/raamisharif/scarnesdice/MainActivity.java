274af3035b9634896ced6ddedbc8833bc942233c
rshar1 <raamisharif@gmail.com>
2018-01-04 14:11:00 -0500

    scarnesDice-milestone3

--- ScarnesDice/app/src/main/java/com/raamisharif/scarnesdice/MainActivity.java
+++ ScarnesDice/app/src/main/java/com/raamisharif/scarnesdice/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,16 +33,31 @@ public class MainActivity extends AppCompatActivity {
         int numRolled = (int) (Math.random() * 6);
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
         imageView.setImageResource(dice[numRolled]);
+
+        TextView label = (TextView) findViewById(R.id.textView);
+        if (numRolled == 0) {
+            userTurnScore = 0;
+            label.setText("Your Score: " + userOverallScore + " Computer Score: " + compOverallScore);
+        } else {
+            userTurnScore += numRolled + 1;
+            label.setText("Your Score: " + userOverallScore + " Computer Score: " + compOverallScore + " Your turn score: " + userTurnScore);
+        }
+
     }
 
     /** Called when the user clicks the hold button */
     public void hold(View view) {
-
+        userOverallScore += userTurnScore;
+        userTurnScore = 0;
+        TextView label = (TextView) findViewById(R.id.textView);
+        label.setText("Your Score: " + userOverallScore + " Computer Score: " + compOverallScore);
     }
 
     /** Called when the user clicks the reset button */
     public void reset(View view) {
-
+        userTurnScore = userOverallScore = compOverallScore = compTurnScore = 0;
+        TextView label = (TextView) findViewById(R.id.textView);
+        label.setText("Your Score: " + userOverallScore + " Computer Score: " + compOverallScore);
     }
 
 }
